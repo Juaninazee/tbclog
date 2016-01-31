a02a2605e86890efce677009f468e30f51afad91
Juaninazee <francois.lef@gmail.com>
2016-01-31 13:06:06 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$string = "Hello World!";
+	
+?>
+
+<html>
+<head>
+<title>TBC LOG</title>
+</head>
+
+<body bgcolor="#000000">
+<center>
+<font face = "Arial" color="#FFFFFF" size="2">
+<?php echo $string ?>
+</font>
+</center>
+</body>
+</html>
